b7cef4ddabbaeee2128a43b88f096e6c03c0eba8
JorPan <jbpanasewicz@gmail.com>
2021-08-16 18:22:16 -0600

    router set up and file structure built

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "../Containers/Header/Header";
+import Home from "../Pages/Home/Home";
+import Music from "../Pages/Music/Music";
+import Media from "../Pages/Media/Media";
+import Merch from "../Pages/Merch/Merch";
+import Tools from "../Pages/Tools/Tools";
 
 function App() {
   return (
