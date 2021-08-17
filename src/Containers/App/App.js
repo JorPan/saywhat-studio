0c13c661fd40e9e278fe03c59ddd4acd1c1e8d0e
JorPan <jbpanasewicz@gmail.com>
2021-08-16 18:10:28 -0600

    react router set up

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route path="/music" component={Music} />
+        <Route path="/media" component={Media} />
+        <Route path="/merch" component={Merch} />
+        <Route path="/tools" component={Tools} />
+        <Redirect to="/" />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <div className="logo-section">
+        <Link className="logo" to="/">
+          SayWhat
+        </Link>
+      </div>
+      <div className="header-link-section">
+        <Link className="header-link" to="/music">
+          Music
+        </Link>
+        <Link className="header-link" to="/media">
+          Media
+        </Link>
+        <Link className="header-link" to="/merch">
+          Merch
+        </Link>
+        <Link className="header-link" to="/tools">
+          Tools
+        </Link>
+      </div>
+    </header>
+  );
 }
