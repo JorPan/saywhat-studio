ef61a32e3eb8795b934c5154ddf07142cc07c772
JorPan <jbpanasewicz@gmail.com>
2021-08-30 17:11:26 -0600

    home page concept

---
+++ src/Auth/Logout.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Auth.css";
+
+export default function Logout() {
+  return <div></div>;
+}
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 
 export default function Home() {
+  const [caption, setCaption] = useState("");
+
+  useEffect(() => {
+    setCaption(
+      "Jordan Panasewicz - also known as Pandasaywhat?! is the brains behind the sounds and visuals of the SayWhat?! project."
+    );
+  }, []);
+
   return (
     <div>
-      <p className="bio">
-        Jordan Panasewicz - also known as Pandasaywhat?! is the brains behind
-        the sounds and visuals of the SayWhat?! project.
-      </p>
+      <img src="" />
+      <p className="bio">{caption}</p>
     </div>
   );
 }
