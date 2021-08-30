a1ab5f82827990a79128e219130519484f97a822
JorPan <jbpanasewicz@gmail.com>
2021-08-30 17:05:10 -0600

    auth files set up

---
+++ src/Auth/Login.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Auth.css";
+
+export default function Login() {
+  return <div></div>;
+}
---
+++ src/Auth/SignUp.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Auth.css";
+
+export default function SignUp() {
+  return <div></div>;
+}
