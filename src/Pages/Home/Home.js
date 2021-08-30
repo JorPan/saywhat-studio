550e22d73d716d973583d744caf707d7f8044c27
JorPan <jbpanasewicz@gmail.com>
2021-08-30 17:03:48 -0600

    clean up

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Home.css";
 
 export default function Home() {
-  //   const [photos, setPhotos] = useState([]);
-
-  //   useEffect(() => {
-  //     setPhotos();
-  //   }, []);
-
   return (
     <div>
       <p className="bio">
-        Jordan Panasewicz - formerly known as Pandasaywhat?! is the brains
-        behind the sounds and visuals of the SayWhat?! project.
+        Jordan Panasewicz - also known as Pandasaywhat?! is the brains behind
+        the sounds and visuals of the SayWhat?! project.
       </p>
     </div>
   );
