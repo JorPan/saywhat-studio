51313c91200a6e95481adc18b5358da2ca6194a5
JorPan <jbpanasewicz@gmail.com>
2021-09-02 14:03:22 -0600

    home page background and bio layout

---
+++ src/Pages/Home/Bio.js
@@ -0,0 +1,13 @@
+const Bio = [
+  {
+    img: "",
+    caption:
+      "Jordan Panasewicz - also known as Pandasaywhat?! is the brains behind the sounds and visuals of the SayWhat?! project.",
+  },
+  { img: "", caption: "" },
+  { img: "", caption: "" },
+  { img: "", caption: "" },
+  { img: "", caption: "" },
+];
+
+export default Bio;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,27 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import Bio from "./Bio";
 import "./Home.css";
 
 export default function Home() {
-  // const [bioPhase, setBioPhase] = useState(0);
-  const [caption, setCaption] = useState("");
-
-  useEffect(() => {
-    setCaption(
-      "Jordan Panasewicz - also known as Pandasaywhat?! is the brains behind the sounds and visuals of the SayWhat?! project."
-    );
-  }, []);
+  const [bioPhase, setBioPhase] = useState(Bio[0]);
 
   return (
     <div class="home-page">
       <div className="bio-select">
-        <div class="biodot biodot1"></div>
-        <div class="biodot biodot2"></div>
-        <div class="biodot biodot3"></div>
-        <div class="biodot biodot4"></div>
-        <div class="biodot biodot5"></div>
+        <div class="biodot biodot1" onClick={() => setBioPhase(Bio[0])}></div>
+        <div class="biodot biodot2" onClick={() => setBioPhase(Bio[1])}></div>
+        <div class="biodot biodot3" onClick={() => setBioPhase(Bio[2])}></div>
+        <div class="biodot biodot4" onClick={() => setBioPhase(Bio[3])}></div>
+        <div class="biodot biodot5" onClick={() => setBioPhase(Bio[4])}></div>
       </div>
       <section>
-        <p className="bio">{caption}</p>
+        <p className="bio">{bioPhase.caption}</p>
       </section>
     </div>
   );
