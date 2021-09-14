55a16d84232563698b8f5f22699d3a2aa9b9ac46
JorPan <jbpanasewicz@gmail.com>
2021-09-14 17:10:03 -0600

    bio captions

--- src/Pages/Home/Bio.js
+++ src/Pages/Home/Bio.js
@@ -4,8 +4,16 @@ const Bio = [
     caption:
       "Jordan Panasewicz - also known as Pandasaywhat?! is the brains behind the sounds and visuals of the SayWhat?! project.",
   },
-  { img: "", caption: "" },
-  { img: "", caption: "" },
+  {
+    img: "",
+    caption:
+      "SayWhat?! music is heavily inspired by jazz, soul, blues, and hip hop, with the obvious touch of bass & EDM",
+  },
+  {
+    img: "",
+    caption:
+      "SayWhat?! visuals are inspired by nature, geometric patterns, abstract arts, and all things made possible by technology",
+  },
   { img: "", caption: "" },
   { img: "", caption: "" },
 ];
